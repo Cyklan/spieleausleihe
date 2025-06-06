ccda25ad6278ad4a8487e6f29b20f613773fba2c
Cyklan <dev@cyklan.de>
2025-06-06 23:19:18 +0200

    remove unused font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,23 +1,23 @@
-import type { Metadata } from "next";
-import "./globals.css";
+import type { Metadata } from "next"
+import "./globals.css"
 
 export const metadata: Metadata = {
   title: "Bad Nauheim Spielt Spieleausleihe",
   description: "Durchsuchen Sie unsere Spieleausleihe und finden Sie das perfekte Spiel.",
-};
+}
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={"antialiased"}
       >
         {children}
       </body>
     </html>
-  );
+  )
 }
